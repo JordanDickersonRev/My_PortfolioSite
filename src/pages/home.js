a4406f2840230efc6aecf68d8e2a30931533f0ff
Jordan Dickerson <jordanmatthewdickerson@gmail.com>
2022-08-20 17:09:26 -0400

    started project page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,19 @@
 import About from './about';
 import Contact from './contact';
+import { Link } from 'react-router-dom';
 
 function Home(){
+
+    let project1Name = 'E Commerce WebSite';
+    let project1Summary = '';
+    let project1Description = '';
+    let project1Video = '';
+
+    let project2Name = 'AMD & Intel Web Scraper';
+    let project2Summary = '';
+    let project2Description = '';
+    let project2Video = '';
+
     return(
         <div>
             <div className='homePage'>
@@ -21,7 +33,30 @@ function Home(){
                 </div>
                 <h2>View Projects</h2>
                 <div id='viewProjects'>
-                    
+                    <div>
+                        <Link className='project'
+                        to={`/projects/${project1Name}`}
+                        state={{ name: project1Name, 
+                                summary: project1Summary,
+                                description: project1Description, 
+                                video: project1Video
+                            }}>
+                            <h3>{project1Name}</h3>
+                            <p>{project1Summary}</p>
+                        </Link>
+                    </div>
+                    <div>
+                        <Link className='project'
+                        to={`/projects/${project2Name}`}
+                        state={{ name: project2Name, 
+                                summary: project2Summary,
+                                description: project2Description, 
+                                video: project2Video
+                            }}>
+                            <h3>{project2Name}</h3>
+                            <p>{project2Summary}</p>
+                        </Link>
+                    </div>
                 </div>
             </div>
             <div id='about'><About/></div>
--- src/pages/projectPage.js
+++ src/pages/projectPage.js
@@ -1,5 +1,17 @@
+import { useLocation } from "react-router-dom"
 
 function ProjectPage(){
+    const location = useLocation();
+    const { name, summary, description, video} = location.state;
+
+    return(
+        <div className="projectPage">
+            <h2>{name}</h2>
+            <p>{summary}</p>
+            <p>{description}</p>
+            
+        </div>
+    )
 
 }
 export default ProjectPage;
