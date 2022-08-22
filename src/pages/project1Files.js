df77bd4d12e0369bb54c82217438a1cc4c183e1b
Jordan Dickerson <jordanmatthewdickerson@gmail.com>
2022-08-22 17:19:19 -0400

    summary of each project is done

--- src/pages/project1Files.js
+++ src/pages/project1Files.js
@@ -1,6 +1,23 @@
 export const project1 = {
-    name : `E Commerce WebSite`,
-    summary : ``,
+    name : `Skate Shop Website`,
+    summary : `SkateHouse is a mock up e-commerce application 
+        where users can buy complete skateboards. Upon entering
+        the application, the user will land on the home page which
+        consist of six links (store, enter the raffle, Twitter,
+        Instagram, Facebook, Snapchat). The header of the application 
+        has five links (log in, sign up, home, store, bag). The 
+        store page has a collection of complete skateboards that the 
+        user can pick from. If the user is looking for a particular 
+        skateboard size, the store has six sizes that they can look 
+        through. When clicking on a skateboard, the user will be taken 
+        to that specific skateboard page where they can do four things 
+        (specify how many boards they want to order, check if the skateboard
+        is left in stock, add it to their bag, view a bigger picture
+        of the skateboard). The users bag shows every skateboard that they 
+        added, the total due, and a list of their favorite skateboards if 
+        the user is logged in. Users can also add skateboards to the bag
+        from their favorites. Scroll down to the foresight section for
+        my future plans.`,
     description : ``,
     difficulties : ``,
     foresight : ``,
--- src/pages/project2Files.js
+++ src/pages/project2Files.js
@@ -1,6 +1,18 @@
 export const project2 = {
     name : `AMD & Intel Web Scraper`,
-    summary : ``,
+    summary : `AMD & Intel Web Scraper is an application that takes the
+        best rated desktops and laptops from AMD and Intel to display
+        that information for the user to see. The header includes a 
+        link to the home page with a hamburger menu that contains four
+        links (AMD laptops, AMD desktops, Intel laptops, Intel desktops).
+        The home page has two links, AMD and Intel. Each link shows the
+        best rated laptops and desktops put together for said brand. 
+        When viewing any link in the application the user can see the 
+        description of the product and the website it came from. Upon 
+        clicking on the description, the user will be redirected to the 
+        product itself. Currently, this application only scrapes from
+        best buy but the goal is to add more websites. Scroll down to the
+        foresight section to read my future plans.`,
     description : ``,
     difficulties : ``,
     foresight : ``,
