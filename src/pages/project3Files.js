53149e18b7989c4c5dd842136e08985d1a4f5506
Jordan Dickerson <jordanmatthewdickerson@gmail.com>
2023-01-12 23:24:50 -0500

    started adding information about my portfolio site under projects

--- src/pages/project3Files.js
+++ src/pages/project3Files.js
@@ -1,3 +1,15 @@
 export const project3 = {
-    
+    name : `This Portfolio Site`,
+    summary :``,
+    responsibilities : [`Owner/Developer`,
+                `Built the database and tables in azure data studio`,
+                `Developed the Apilayer, Businesslayer, Repolayer, and Modelslayer using the repository pattern`,
+                `Developed every action method for the controllers`,
+                `Developed DQL and DML to query or manipulate SQL in the database depending on the action method`,
+                `Built JWT Authentication so that a token is returned every time the client logs in`,
+                `Tested API with Swagger`,
+                `Built custom logger for Repolayer`],
+    technologies : ``,
+    video : ``,
+    code : `https://github.com/JordanDickersonRev/My_PortfolioSite`
 };
