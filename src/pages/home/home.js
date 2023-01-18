a285edfc9cd7de3d1127d3804edc8f6ad015f912
Jordan Dickerson <jordanmatthewdickerson@gmail.com>
2023-01-17 22:48:50 -0500

    skills section rework is done

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -6,15 +6,7 @@ import {project2} from '../project2/project2files';
 import {project3} from '../project3/project3files';
 import './home.css';
 
-function Home(){
-
-   /* const skills = `These are the languages, tools, and technologies I use to bring products to life.
-        This list only reflects the skills I use the most. View my resume in the about section 
-        to see more of my skills. I can learn any language, tools, or technology, I do not have 
-        preferences.`;
-
-        <p>{skills}</p>*/
-  
+function Home(){  
     return(
         <div>
             <div className='homePage' id='mySection'>
@@ -25,7 +17,7 @@ function Home(){
                     </div>
                     <div className='profile'>
                         <h2>Hi, I'm Jordan</h2>
-                        <p>My goal is to become a web developer where I can deliver business value while also leveling up my skills as a developer.</p>
+                        <p>My goal is to become a Software Developer where I can deliver business value while also leveling up my skills as a developer.</p>
                     </div>
                 </div>
                 <div className='skills'>
@@ -33,36 +25,21 @@ function Home(){
                         <h2>Skills</h2> 
                     </div>
                     <div className='skillsSection2'>
-                        <h3>Back-End Technologies</h3>
                         <div>
-                            <p>C#</p>
-                            <p>ASP.NET</p>
-                            <p>ADO.NET</p>
-                            <p>.NET</p>
-                            <p>SQL Database</p>
+                            <h3>Back-End Technologies</h3>
+                            <p>C# | ASP.NET | ADO.NET | .NET | SQL Database</p>
                         </div>
-                        <h3>Front-End Technologies</h3>
                         <div>
-                            <p>React</p>
-                            <p>HTML</p>
-                            <p>CSS</p>
-                            <p>JavaScript</p>
+                            <h3>Front-End Technologies</h3>
+                            <p>React | HTML | CSS | JavaScript</p>
                         </div>
-                        <h3>Front-End Technologies</h3>
                         <div>
-                            <p>Git</p>
-                            <p>SDLC</p>
-                            <p>Microsoft Azure</p>
-                            <p>MySQL Workbench</p>
-                            <p>Unix/Linux Commands</p>
+                            <h3>Software Developer Skills</h3>
+                            <p>Git | SDLC | Microsoft Azure | MySQL Workbench | Unix/Linux Commands</p>
                         </div>
-                        <h3>Other Programming Languages</h3>
                         <div>
-                            <p>Java</p>
-                            <p>Python</p>
-                            <p>C++</p>
-                            <p>C</p>
-                            <p>Unix/Linux Shell Scripting</p>
+                            <h3>Other Programming Languages</h3>
+                            <p>Java | Python | C++ | C | Unix/Linux Shell Scripting</p>
                         </div>
                     </div>
                     <div id='viewProjects'></div>
